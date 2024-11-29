bdd26206d562947bd95fbdda564c251253d63427
GabrielLima2803 <gabriellima2803@gmail.com>
2024-11-29 10:25:56 -0300

    FEAT: Adição self em nota

--- core/models/nota.py
+++ core/models/nota.py
@@ -6,3 +6,12 @@ class Nota(models.Model):
     aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE, related_name='notas')
     disciplina = models.ForeignKey(Disciplina, on_delete=models.CASCADE, related_name='notas')
     trimestre = models.ForeignKey(Trimestre, on_delete=models.CASCADE, related_name='notas')
+
+
+    def __self__(self):
+        return f"{self.aluno.nome} - {self.disciplina.nome} - {self.trimestre.nome}"
+    
+    def save(self, *args, **kwargs):
+        if self.turma.conselho.ativado:
+            raise ValueError(f"Não é possível enviar notas para a turma {self.turma.nome} enquanto o conselho está ativado.")
+        super().save(*args, **kwargs)
