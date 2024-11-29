d4d64a227788137b02c02d06da59000faa7a1ba7
GabrielLima2803 <gabriellima2803@gmail.com>
2024-11-29 14:18:58 -0300

    feat: Criando Views Personalizadas

--- core/views/aluno.py
+++ core/views/aluno.py
@@ -1,8 +1,21 @@
 from rest_framework.viewsets import ModelViewSet
-
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from core.models import Aluno, Nota, Trimestre
 from core.models import Aluno
-from core.serializers import AlunoSerializer
+from core.serializers import AlunoSerializer, NotaSerializer
 
 class AlunoViewSet(ModelViewSet):
     queryset = Aluno.objects.all()
     serializer_class = AlunoSerializer
+
+
+    @action(detail=False, methods=['get'])
+    def inferior_seis(self, request):
+        alunos_com_nota_baixa = Aluno.objects.filter(
+            notas__nota__lt=6
+        ).distinct()
+
+        serializer = self.get_serializer(alunos_com_nota_baixa, many=True)
+        return Response(serializer.data)
+
--- core/views/nota.py
+++ core/views/nota.py
@@ -1,7 +1,27 @@
-from rest_framework.viewsets import ModelViewSet
-from core.models import Nota
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import viewsets
+from core.models import Nota, Trimestre
 from core.serializers import NotaSerializer
 
-class NotaViewSet(ModelViewSet):
+class NotaViewSet(viewsets.ModelViewSet):
     queryset = Nota.objects.all()
     serializer_class = NotaSerializer
+
+    @action(detail=False, methods=['get'])
+    def notas_por_trimestre(self, request):
+        trimestre_id = request.query_params.get('trimestre', None)
+
+        if not trimestre_id:
+            return Response({"detail": "Trimestre não fornecido."}, status=400)
+
+        try:
+            trimestre = Trimestre.objects.get(id=trimestre_id)
+        except Trimestre.DoesNotExist:
+            return Response({"detail": "Trimestre não encontrado."}, status=404)
+
+        notas_no_trimestre = Nota.objects.filter(trimestre=trimestre)
+
+        notas_serializer = NotaSerializer(notas_no_trimestre, many=True)
+
+        return Response(notas_serializer.data)
