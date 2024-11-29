a6d3510f416b047d3c97fdb46e90513cd476bc89
GabrielLima2803 <gabriellima2803@gmail.com>
2024-11-29 09:39:40 -0300

    Fix: Erro de importação

--- core/models/aluno.py
+++ core/models/aluno.py
@@ -5,7 +5,7 @@ class Aluno(models.Model):
     matricula = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
     cpf = models.CharField(max_length=100)
-    foto = models.ForeignKey('Image', on_delete=models.SET_NULL, null=True, blank=True, default=None, related_name='+')
+    foto = models.ForeignKey('uploader.Image', on_delete=models.SET_NULL, null=True, blank=True, default=None, related_name='+')
 
     def __str__(self):
         return f"{self.nome} - {self.matricula}"
