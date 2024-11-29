cc30417311337ce32b8da8b963c99d30443179ca
GabrielLima2803 <gabriellima2803@gmail.com>
2024-11-29 18:31:41 -0300

    feat: Calcular Media da Turma

--- core/views/turma.py
+++ core/views/turma.py
@@ -7,3 +7,9 @@ from core.serializers import TurmaSerializer
 class TurmaViewSet(ModelViewSet):
     queryset = Turma.objects.all().order_by("id")
     serializer_class = TurmaSerializer
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        for turma in queryset:
+            turma.calcular_media_da_turma()
+        return queryset
